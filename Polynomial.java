9a447dcc9ff2e6461345cf8d8c90f22bf9b01033
JasjotBenipal <jasjot.benipal11@gmail.com>
2023-06-04 22:33:51 -0400

    Updated Polynomial.java
    Updated Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -7,32 +7,40 @@ public class Polynomial{
 
 	// No argument constructor
 	public Polynomial(){
-		coefficients = new double[0];
+		this.coefficients = new double[0];
 	}
 
 	// 1 argument constructor
 	public Polynomial(double[] polyInput){
-		coefficients = polyInput;
+		this.coefficients = polyInput;
 	}
 
 	// Add method
 	public Polynomial add(Polynomial polyArg){
-		for (int i = 0; i < polyArg.coefficients.length; i++) {
-			if (i >= coefficients.length){
-				polyArg.coefficients[i] = polyArg.coefficients[i];					
-			}
-			else {
-			polyArg.coefficients[i] = coefficients[i] + polyArg.coefficients[i];
-			}
+		int polyLength = polyArg.coefficients.length;
+
+		if (this.coefficients.length > polyLength) {
+			polyLength = this.coefficients.length;
 		}
-		return polyArg;
+
+		double[] newPoly = new double[polyLength];
+
+		for (int i = 0; i < this.coefficients.length; i++) {
+			newPoly[i] = this.coefficients[i];
+		}
+
+		for (int i = 0; i < polyArg.coefficients.length; i++) {
+			newPoly[i] += polyArg.coefficients[i];
+		}
+		
+		return new Polynomial(newPoly);
 	}
 
 	// Evaluate Method
 	public double evaluate(double x){
 		double result = 0;
-		for (int i = 0; i < coefficients.length; i++) {
-			result = result + (Math.pow(x, i) * coefficients[i]);
+		for (int i = 0; i < this.coefficients.length; i++) {
+			result = result + (Math.pow(x, i) * this.coefficients[i]);
 		}
 		return result;
 	}
@@ -42,12 +50,3 @@ public class Polynomial{
 		return evaluate(y) == 0;
 	}
 }
-
-
-
-
-
-
-
-
-	
